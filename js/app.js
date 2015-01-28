fa9e7750c2d7e1cd0da34dbc1805e818993f952c
James Conkling <james.lane.conkling@gmail.com>
2015-01-28 12:46:25 -0500

    add mapbox map

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $(document).foundation();
 
 (function(){
-  console.log('app.js executed');
+  var report = {
+    init: function(){
+      L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
+      report.map = L.mapbox.map('map', 'james-lane-conkling.5630f970',{
+        // center: [],
+        zoom: 6,
+        minZoom: 4,
+        maxZoom: 16,
+        scrollWheelZoom: false
+      });
+    }
+  };
+
+  window.report = report;
+  report.init();
 })();
